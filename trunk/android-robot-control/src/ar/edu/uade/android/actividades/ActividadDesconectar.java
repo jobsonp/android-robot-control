3cfa571ff9fad7e031d4c85a55888658421bebfa
robertoestivill@gmail.com <robertoestivill@gmail.com@b9c6b212-2df8-11de-bd8d-4b29057232bf>
2009-09-07 03:09:01 +0000

    Testing del trackball en las Actividades de las webcams.

--- trunk/android-robot-control/src/ar/edu/uade/android/actividades/ActividadDesconectar.java
+++ trunk/android-robot-control/src/ar/edu/uade/android/actividades/ActividadDesconectar.java
@@ -3,6 +3,8 @@ package ar.edu.uade.android.actividades;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+import ar.edu.uade.android.R;
 import ar.edu.uade.android.constantes.Constantes;
 
 public class ActividadDesconectar
@@ -15,6 +17,8 @@ public class ActividadDesconectar
         // 1) Abrir aviso de loading
         // 2) Desconectar y hacer toda la cochinada
         // 3) Cerrar aviso de loading
+        
+        Toast.makeText( this, R.string.toast_desconectando, Toast.LENGTH_SHORT ).show();
         
         startActivity( new Intent( Constantes.INTENT_PANTALLA_CONEXION ) );
     }
--- trunk/android-robot-control/src/ar/edu/uade/android/actividades/ActividadPantallaWebcamBrazo.java
+++ trunk/android-robot-control/src/ar/edu/uade/android/actividades/ActividadPantallaWebcamBrazo.java
@@ -1,6 +1,8 @@
 package ar.edu.uade.android.actividades;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.TextView;
 import ar.edu.uade.android.R;
 
 
@@ -16,6 +18,49 @@ public class ActividadPantallaWebcamBrazo
         setContentView( R.layout.webcam_brazo );
     }
 
+    @Override
+    public boolean onTrackballEvent( MotionEvent event )
+    {
+        boolean eventHandled = false;
+        
+        TextView trackballClick = (TextView) this.findViewById( R.id.TrackballClick );
+        TextView posX = (TextView) this.findViewById( R.id.pos_x );
+        TextView posY = (TextView) this.findViewById( R.id.pos_y );
+        
+        trackballClick.setText( "" );
+        posX.setText( "" );
+        posY.setText( "" );
+        
+        if( event.getAction() == MotionEvent.ACTION_DOWN )
+        {
+            trackballClick.setText( "Down!" );
+
+            eventHandled = true;
+        }
+        else if( event.getAction() == MotionEvent.ACTION_UP )
+        {
+            trackballClick.setText( "Up!" );
+
+            eventHandled = true;
+        }
+        else if( event.getAction() == MotionEvent.ACTION_CANCEL )
+        {
+            trackballClick.setText( "Cancel!" );
+
+            eventHandled = true;
+        }
+        else if( event.getAction() == MotionEvent.ACTION_MOVE )
+        {
+            trackballClick.setText( "Move!" ); 
+            posX.setText( String.valueOf(  event.getX( ) ) );
+            posY.setText( String.valueOf(  event.getY( ) ) );
+            
+            eventHandled = true;
+        }
+        
+        return eventHandled;
+    }
+    
     @Override
     protected boolean isRouteDisplayed()
     {
