abc50d1e6247bf7ffa95aeae537119411d5f4c71
robertoestivill <robertoestivill@gmail.com>
2009-09-26 03:48:27 +0000

    - Agrego la ip del robot del Labot al conectar.xml para no tener que tipearla a mano cada vez que hay que correr la aplicacion. - Agrego classes de implementacion de MJPEG. MjpegInputStream y MjpegView - Tambien agregue el streaming como fondo de las activities principal y del brazo. - Agregue relative layouts para poner la info del trackball en un overlayer sobre el streaming.

--- android-robot-control/src/ar/edu/uade/android/actividades/ActividadPantallaWebcamPrincipal.java
+++ android-robot-control/src/ar/edu/uade/android/actividades/ActividadPantallaWebcamPrincipal.java
@@ -2,21 +2,38 @@ package ar.edu.uade.android.actividades;
 
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 import ar.edu.uade.android.R;
-
+import ar.edu.uade.android.mjpeg.MjpegInputStream;
+import ar.edu.uade.android.mjpeg.MjpegView;
 
 public class ActividadPantallaWebcamPrincipal
-    extends ActividadPantallaAbstract 
+    extends ActividadPantallaAbstract
 {
-    
+
+    private MjpegView mv;
+
     @Override
     protected void onCreate( Bundle savedInstanceState )
     {
         // TODO Auto-generated method stub
         super.onCreate( savedInstanceState );
-        
+
+        // sample public cam
+        String URL = "http://10.0.2.2:1234";
+
+        requestWindowFeature( Window.FEATURE_NO_TITLE );
+        getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN );
+
         setContentView( R.layout.webcam_principal );
+
+        mv = (MjpegView) this.findViewById( R.id.mjpeg_view_main );
+
+        mv.setSource( MjpegInputStream.read( URL ) );
+        mv.setDisplayMode( MjpegView.SIZE_BEST_FIT );
+        mv.showFps( true );
     }
 
     @Override
@@ -26,23 +43,35 @@ public class ActividadPantallaWebcamPrincipal
 
         TextView posX = (TextView) this.findViewById( R.id.pos_x );
         TextView posY = (TextView) this.findViewById( R.id.pos_y );
-        
-        if( event.getAction() == MotionEvent.ACTION_MOVE )
+
+        if ( event.getAction() == MotionEvent.ACTION_MOVE )
         {
-            posX.setText( String.valueOf(  event.getX( ) ) );
-            posY.setText( String.valueOf(  event.getY( ) ) );
-            
+            posX.setText( "X: " + String.valueOf( event.getX() ) );
+            posY.setText( "Y: " + String.valueOf( event.getY() ) );
+
             eventHandled = true;
         }
-        
+
         return eventHandled;
     }
-    
+
     @Override
     protected boolean isRouteDisplayed()
     {
         return false;
     }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        mv.stopPlayback();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        mv.stopPlayback();
+    }
 }
-
-
