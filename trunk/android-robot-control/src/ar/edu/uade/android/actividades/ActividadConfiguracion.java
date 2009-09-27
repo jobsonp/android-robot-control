27b56c2f5a2f1664593f4f7d58509c4d04c5f48a
robertoestivill@gmail.com <robertoestivill@gmail.com@b9c6b212-2df8-11de-bd8d-4b29057232bf>
2009-09-27 19:20:19 +0000

    -Borre la declaracion de todos los servicios del AndroidManifest.xml -Agregue la ActivityConfiguracion. -Agregue una opcion al menu en ActivityAbstract para lanzar la activity de configuracion -Agregue arrays.xml y configuracion.xml. -Agregue muchos strings que se usan en la pantalla de configuracion.

---
+++ trunk/android-robot-control/src/ar/edu/uade/android/actividades/ActividadConfiguracion.java
@@ -0,0 +1,17 @@
+package ar.edu.uade.android.actividades;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import ar.edu.uade.android.R;
+
+public class ActividadConfiguracion
+    extends PreferenceActivity
+{    
+    @Override
+    public void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+
+        addPreferencesFromResource( R.xml.configuracion ); 
+    }
+}
